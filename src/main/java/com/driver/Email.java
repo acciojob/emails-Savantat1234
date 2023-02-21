9025f36916aa23003274f92253a476bf1ebf2498
Anjali s <savantanjali0@gmail.com>
2023-02-21 12:17:09 +0530

    email

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -19,11 +19,59 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
+
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(oldPassword.equals(password)){
+            if(isValid(newPassword)){
+                this.password = newPassword;
+                System.out.println("Password changed successfully");
+            }
+            else{
+                System.out.println("The new password is not valid");
+            }
+        }
+        else{
+            System.out.println("The given password does not match the current password");
+        }
+
+    }
+    private boolean isValid(String password){
+        boolean capitalLetter = false;
+        boolean smallLetter = false;
+        boolean digit = false;
+        boolean  specialCharactor = false;
+
+        if(password.length() < 8){
+            return false;
+        }
+        for(int i=0; i<password.length(); i++){
+            char ch = password.charAt(i);
+            if((ch >= 'A') && (ch <= 'Z')){
+                capitalLetter = true;
+            }
+            if((ch >= 'a') && (ch <= 'z')){
+                smallLetter = true;
+            }
+            if((ch >= '0') && (ch <= '9')){
+                digit = true;
+            }
+            else {
+                specialCharactor = true;
+            }
+        }
+
+        if(capitalLetter && smallLetter && digit && specialCharactor){
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 }
